e4e01c47d879bb5000285d7747fbe2f035759c01
Tarek salah <64745962+tarek-98@users.noreply.github.com>
2023-11-29 18:59:05 +0300

    Add files via upload

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./movie.css";
 import { useParams } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Movie = () => {
   const [currentMovieDetail, setMovie] = useState();
@@ -22,115 +23,151 @@ const Movie = () => {
   return (
     <div className="movie">
       <div className="movie__intro">
-        <img
-          className="movie__backdrop"
-          src={`https://image.tmdb.org/t/p/original${
-            currentMovieDetail ? currentMovieDetail.backdrop_path : ""
-          }`}
-        />
+        <Container>
+          <Row>
+            <Col>
+              <img
+                className="movie__backdrop"
+                src={`https://image.tmdb.org/t/p/original${
+                  currentMovieDetail ? currentMovieDetail.backdrop_path : ""
+                }`}
+              />
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="movie__detail">
-        <div className="movie__detailLeft">
-          <div className="movie__posterBox">
-            <img
-              className="movie__poster"
-              src={`https://image.tmdb.org/t/p/original${
-                currentMovieDetail ? currentMovieDetail.poster_path : ""
-              }`}
-            />
-          </div>
-        </div>
-        <div className="movie__detailRight">
-          <div className="movie__detailRightTop">
-            <div className="movie__name">
-              {currentMovieDetail ? currentMovieDetail.original_title : ""}
-            </div>
-            <div className="movie__tagline">
-              {currentMovieDetail ? currentMovieDetail.tagline : ""}
-            </div>
-            <div className="movie__rating">
-              {currentMovieDetail ? currentMovieDetail.vote_average : ""}{" "}
-              <i class="fas fa-star" />
-              <span className="movie__voteCount">
-                {currentMovieDetail
-                  ? "(" + currentMovieDetail.vote_count + ") votes"
-                  : ""}
-              </span>
-            </div>
-            <div className="movie__runtime">
-              {currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}
-            </div>
-            <div className="movie__releaseDate">
-              {currentMovieDetail
-                ? "Release date: " + currentMovieDetail.release_date
-                : ""}
-            </div>
-            <div className="movie__genres">
-              {currentMovieDetail && currentMovieDetail.genres
-                ? currentMovieDetail.genres.map((genre) => (
-                    <Fragment>
-                      <span className="movie__genre" id={genre.id}>
-                        {genre.name}
-                      </span>
-                    </Fragment>
-                  ))
-                : ""}
-            </div>
-          </div>
-          <div className="movie__detailRightBottom">
-            <div className="synopsisText">Synopsis</div>
-            <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
-          </div>
-        </div>
+        <Container>
+          <Row>
+            <Col sm={12}>
+              <div className="movie__detailLeft">
+                <div className="movie__posterBox">
+                  <img
+                    className="movie__poster"
+                    src={`https://image.tmdb.org/t/p/original${
+                      currentMovieDetail ? currentMovieDetail.poster_path : ""
+                    }`}
+                  />
+                </div>
+              </div>
+            </Col>
+            <Col sm={12}>
+              <div className="movie__detailRight">
+                <div className="movie__detailRightTop">
+                  <div className="movie__name">
+                    {currentMovieDetail
+                      ? currentMovieDetail.original_title
+                      : ""}
+                  </div>
+                  <div className="movie__tagline">
+                    {currentMovieDetail ? currentMovieDetail.tagline : ""}
+                  </div>
+                  <div className="movie__rating">
+                    {currentMovieDetail ? currentMovieDetail.vote_average : ""}{" "}
+                    <i class="fas fa-star" />
+                    <span className="movie__voteCount">
+                      {currentMovieDetail
+                        ? "(" + currentMovieDetail.vote_count + ") votes"
+                        : ""}
+                    </span>
+                  </div>
+                  <div className="movie__runtime">
+                    {currentMovieDetail
+                      ? currentMovieDetail.runtime + " mins"
+                      : ""}
+                  </div>
+                  <div className="movie__releaseDate">
+                    {currentMovieDetail
+                      ? "Release date: " + currentMovieDetail.release_date
+                      : ""}
+                  </div>
+                  <div className="movie__genres">
+                    {currentMovieDetail && currentMovieDetail.genres
+                      ? currentMovieDetail.genres.map((genre) => (
+                          <Fragment>
+                            <span className="movie__genre" id={genre.id}>
+                              {genre.name}
+                            </span>
+                          </Fragment>
+                        ))
+                      : ""}
+                  </div>
+                </div>
+                <div className="movie__detailRightBottom">
+                  <div className="synopsisText">Synopsis</div>
+                  <div>
+                    {currentMovieDetail ? currentMovieDetail.overview : ""}
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="movie__links">
-        <div className="movie__heading">Useful Links</div>
-        {currentMovieDetail && currentMovieDetail.homepage && (
-          <a
-            href={currentMovieDetail.homepage}
-            target="_blank"
-            style={{ textDecoration: "none" }}
-          >
-            <p>
-              <span className="movie__homeButton movie__Button">
-                Homepage <i className="newTab fas fa-external-link-alt"></i>
-              </span>
-            </p>
-          </a>
-        )}
-        {currentMovieDetail && currentMovieDetail.imdb_id && (
-          <a
-            href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id}
-            target="_blank"
-            style={{ textDecoration: "none" }}
-          >
-            <p>
-              <span className="movie__imdbButton movie__Button">
-                IMDb<i className="newTab fas fa-external-link-alt"></i>
-              </span>
-            </p>
-          </a>
-        )}
-      </div>
-      <div className="movie__heading">Production companies</div>
-      <div className="movie__production">
-        {currentMovieDetail &&
-          currentMovieDetail.production_companies &&
-          currentMovieDetail.production_companies.map((company) => (
-            <Fragment>
-              {company.logo_path && (
-                <span className="productionCompanyImage">
-                  <img
-                    className="movie__productionComapany"
-                    src={
-                      "https://image.tmdb.org/t/p/original" + company.logo_path
-                    }
-                  />
-                  <span>{company.name}</span>
-                </span>
+        <Container>
+          <Row>
+            <Col className="col">
+              <div className="movie__heading">Useful Links</div>
+              {currentMovieDetail && currentMovieDetail.homepage && (
+                <a
+                  href={currentMovieDetail.homepage}
+                  target="_blank"
+                  style={{ textDecoration: "none" }}
+                >
+                  <p>
+                    <span className="movie__homeButton movie__Button">
+                      Homepage{" "}
+                      <i className="newTab fas fa-external-link-alt"></i>
+                    </span>
+                  </p>
+                </a>
               )}
-            </Fragment>
-          ))}
+              {currentMovieDetail && currentMovieDetail.imdb_id && (
+                <a
+                  href={
+                    "https://www.imdb.com/title/" + currentMovieDetail.imdb_id
+                  }
+                  target="_blank"
+                  style={{ textDecoration: "none" }}
+                >
+                  <p>
+                    <span className="movie__imdbButton movie__Button">
+                      IMDb<i className="newTab fas fa-external-link-alt"></i>
+                    </span>
+                  </p>
+                </a>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="movie__heading text-center">Production companies</div>
+      <div className="movie__production">
+        <Container>
+          <Row>
+            <Col className="col">
+              {currentMovieDetail &&
+                currentMovieDetail.production_companies &&
+                currentMovieDetail.production_companies.map((company) => (
+                  <Fragment>
+                    {company.logo_path && (
+                      <span className="productionCompanyImage">
+                        <img
+                          className="movie__productionComapany"
+                          src={
+                            "https://image.tmdb.org/t/p/original" +
+                            company.logo_path
+                          }
+                        />
+                        <span>{company.name}</span>
+                      </span>
+                    )}
+                  </Fragment>
+                ))}
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
